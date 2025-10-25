69a81ff5526731cfc31d7d2f1a7dc47bd3ff4c0e
Yousef_Mslmany <mslmanyywsf0@gmail.com>
2025-10-26 00:35:12 +0300

    Add files via upload

---
+++ Fundamentals /Coding/Algorithm Solutions Level 1/Problem 1.cpp
@@ -0,0 +1,17 @@
+//This is a soultion to the Problem #1
+#include <iostream>
+using namespace std;
+
+void PrintYourName(string Name)
+{
+    cout << "Your Name is: " << Name << endl;
+};
+
+
+
+//This is a Program to print your name on screen.
+int main()
+{
+    PrintYourName("Youesf_Mslmany");
+    return 0;
+}
